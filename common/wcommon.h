26179ea227978618a9be1d833608c1bc6497de17
Simon <hemengsi123@126.com>
2015-01-08 15:38:03 +0800

    readme.md edit

---
+++ common/wcommon.h
@@ -0,0 +1,44 @@
+#ifndef _WIN32_COMMON_ONE_
+#define _WIN32_COMMON_ONE_
+// 2015.01.08
+#include <io.h>
+#include <Shlwapi.h>
+
+// ======== macro ===========
+#define lengthof(x) (sizeof((x))/sizeof(*(x)))
+
+// ========= function =========
+bool WINAPI IsDirectory(LPCTSTR lpPath)
+{
+	return GetFileAttributes(lpPath) == FILE_ATTRIBUTE_DIRECTORY;
+}
+bool IsExits(LPCTSTR lpPath)
+{
+	return _waccess(lpPath, 0) != -1;
+}
+void DbgPrintf(LPCTSTR lpStrFormt, ...)
+{
+#ifdef _DEBUG
+	TCHAR szBuff[512] = {0};
+	int realLen = wnsprintf(szBuff, lengthof(szBuff), L"[Tip]");
+	va_list args;
+	va_start(args, lpStrFormt);
+	realLen += wvnsprintf(&szBuff[realLen], lengthof(szBuff) - 2, lpStrFormt, args);
+	va_end(args);
+	if(realLen < 0)
+		::OutputDebugString(L"lpStrFormt error.");
+	else if( realLen >= lengthof(szBuff))
+	{
+		wnsprintf(szBuff, lengthof(szBuff), L"[Tip]debug message too long: %d\n", realLen);
+		::OutputDebugString(szBuff);
+	}
+	else
+	{
+		::OutputDebugString(szBuff);
+	}
+	// _tcscat(szBuff, L"\n");
+#endif
+}
+
+
+#endif
--- test1/main.cpp
+++ test1/main.cpp
@@ -1,39 +1,51 @@
 
 #include <windows.h>
-#include <io.h>
 #include <stdio.h>
+// #incldue <wchar.h>
+#include <tchar.h>
 
-bool WINAPI IsDirectory(LPCTSTR lpPath)
-{
-	return GetFileAttributes(lpPath) == FILE_ATTRIBUTE_DIRECTORY;
-}
-bool IsExits(LPCTSTR lpPath)
-{
-	return _waccess(lpPath, 0) != -1;
-}
+#include "../common/wcommon.h"
+
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPreInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
-	wchar_t * file = L"E:\\Simon\\projects\\flair61\\zipsig1.exe";
+	wchar_t * file = L"E:\\Simon\\projects\\flair61\\zipsig.exe";
 	if( IsExits(file) )
 	{
 		if( IsDirectory(file) )
 		{
-			::MessageBox(NULL, L"is a directory", L"test1", MB_OK);
+			DbgPrintf(L"%s is a directory\n", file);
 		}
 		else
 		{
-			::MessageBox(NULL, L"isn't a directory", L"test1", MB_OK);
-			HANDLE hFile = ::CreateFile(file, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-			if( hFile == INVALID_HANDLE_VALUE)
+			DbgPrintf(L"isn't a directory");
+			HANDLE hInFile = ::CreateFile(file, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+			if( hInFile != INVALID_HANDLE_VALUE)
 			{
-				OutputDebugString(L"the file isn't exist");
+				// HANDLE hOutFile = ::CreateFile(
+				TCHAR szOutFile[MAX_PATH] = {0};
+				TCHAR * pName = _tcsrchr(file, L'\\');
+				if( pName != NULL)
+				{
+					DbgPrintf(++pName);
+				}
+				DWORD fileSizeHigh = 0;
+				DWORD fileSizeLow  = ::GetFileSize(hInFile, &fileSizeHigh);
+				if(  fileSizeLow ==  INVALID_FILE_SIZE)
+				{
+					DbgPrintf(L"error: GetFileSize failed\n");
+				}
+				else
+				{
+					DbgPrintf(L"high:%d\tlow:%d", fileSizeHigh, fileSizeLow);
+				}
+				::CloseHandle(hInFile);
 			}
+			
 		}
 	}
 	else
 	{
-		::MessageBox(NULL, L"the path isn't exist", L"test1", MB_OK);
-		OutputDebugString(L"the file isn't exist");
+		DbgPrintf(L"the file isn't exist");
 	}
 	
 	return 0;
