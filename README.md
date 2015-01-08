76e97f735f2aa8c8d622b4f81a9320b7556a355e
Simon <hemengsi123@126.com>
2015-01-08 18:12:59 +0800

    tmp

--- README.md
+++ README.md
@@ -5,28 +5,38 @@ readme.md 文件 [编写规则](https://help.github.com/articles/markdown-basics
 # 1. char wchar_t TCHAR 介绍
 #### // add 2015.01.08
 
-由于字符编码的不同，在C++中有三种对于字符类型：char, wchar_t , TCHAR；
+由于字符编码的不同，在C++中有三种对于字符类型：char, wchar_t , TCHAR。
 TCHAR不能算作一种类型，他紧紧是一个宏, 在使用多字节编码时被定义成char，在Unicode编码时定义成wchar_t
 ```
 type                Meaning in MBCS builds          Meaning in Unicode builds
 WCHAR                 wchar_t                             wchar_t
-LPSTR                 char*                                char*
-LPCSTR                const char*                        const char*
-LPWSTR                wchar_t*                           wchar_t*
-LPCWSTR               const wchar_t*                    const wchar_t* 
-TCHAR                 char                                  wchar_t
-LPTSTR                TCHAR*                            TCHAR*
-LPCTSTR               const TCHAR*                     const TCHAR* 
+LPSTR                 char*                               char*
+LPCSTR                const char*                         const char*
+LPWSTR                wchar_t*                            wchar_t*
+LPCWSTR               const wchar_t*                      const wchar_t* 
+TCHAR                 char                                wchar_t
+LPTSTR                TCHAR*                              TCHAR*
+LPCTSTR               const TCHAR*                        const TCHAR* 
 
 常用的字符串处理函数和宏:
 1、str 开头的 函数 处理SBCS字符串
 2、wcs 开头的 函数 处理宽字符串，wcs是宽字符串的英文缩写 \#include <wchar.h>
    所有的unicode函数均以wcs开头。若要调用Unicode函数，只需用前缀wcs来取代ANSI字符串函数的前缀str即可。
    对于每一个标准的ANSI C字符串函数，基本都有等价的unicode函数.
-3、_mbs 开头的 函数 处理DBCS字符串
+3、_mbs 开头的 函数 处理DBCS（双字节字符集）字符串
 4、_tcs 开头的 宏 配合TCHAR使用 #include <tchar.h>
    根据预定义分别扩展为str wcs _mbs， 见后面的描述。
 5、l开头的windows自带的宽字符处理函数
 6、大小写兼有的 windows自带的宽字符处理函数
 
+###### L 与 _T 区别
+L"" 转换成unicode的字符串
+_T 根据是否定义了_UNICODE，选择ANCI或UNICODE字符集(_TEXT同其一样)
+
+###### 在C语言中，如何输出百分号呢？
+只要在格式控制符里输入2个 %% 就可以了: printf("%d%%\n",10); \\10%
+
+###### INT 与 DWORD 区别
+INT 代表int，int会随着机器位数的不同而发生变化，比如在16位机上为16为，在32位机上为32位，在64位机上为64位。
+DWORD 代表 unsigned long
 ```
--- test1/main.cpp
+++ test1/main.cpp
@@ -8,7 +8,7 @@
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPreInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
-	wchar_t * file = L"E:\\Simon\\projects\\flair61\\zipsig.exe";
+	wchar_t * file = _T("E:\\Simon\\projects\\flair61\\zipsig.exe");
 	if( IsExits(file) )
 	{
 		if( IsDirectory(file) )
@@ -17,28 +17,75 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPreInstance, _In_ L
 		}
 		else
 		{
-			DbgPrintf(L"isn't a directory");
+			dbg_log(_T("isn't a directory"));
 			HANDLE hInFile = ::CreateFile(file, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 			if( hInFile != INVALID_HANDLE_VALUE)
 			{
-				// HANDLE hOutFile = ::CreateFile(
+				TCHAR tmpPath[MAX_PATH] = {0};
+				int npath = 0;
+				// 得到 tmp 环境变量 的值
+				if( !(npath = ::GetEnvironmentVariable(_T("tmp"), tmpPath, lengthof(tmpPath))) )
+				{
+					dbg_log(_T("leng: %d"), npath);
+					if( !(npath = ::GetEnvironmentVariable(_T("temp"), tmpPath, lengthof(tmpPath))) )
+					{
+						dbg_log(_T("the %%tmp%% is not exist!"));
+						return 0;
+					}
+					
+				}
 				TCHAR szOutFile[MAX_PATH] = {0};
 				TCHAR * pName = _tcsrchr(file, L'\\');
 				if( pName != NULL)
 				{
-					DbgPrintf(++pName);
-				}
-				DWORD fileSizeHigh = 0;
-				DWORD fileSizeLow  = ::GetFileSize(hInFile, &fileSizeHigh);
-				if(  fileSizeLow ==  INVALID_FILE_SIZE)
-				{
-					DbgPrintf(L"error: GetFileSize failed\n");
+					// 得到临时文件名字
+					wnsprintf(&tmpPath[npath], lengthof(tmpPath)-npath-1, _T("\\%s.tmp"), ++pName);
 				}
 				else
 				{
-					DbgPrintf(L"high:%d\tlow:%d", fileSizeHigh, fileSizeLow);
+					wnsprintf(&tmpPath[npath], lengthof(tmpPath)-npath-1, _T("\\%s.tmp"), file);
 				}
+				dbg_log(_T("%s"), tmpPath);
+				// 创建临时文件
+				HANDLE hTmpFile = ::CreateFile(tmpPath, GENERIC_WRITE, 0, NULL, OPEN_ALWAYS,0,NULL);
+				if( hTmpFile == INVALID_HANDLE_VALUE)
+				{
+					dbg_log(_T("创建临时文件失败: %x"), ::GetLastError() );
+					::CloseHandle(hInFile);
+					return -1;
+				}
+				// 复制文件
+				char buff[4096] = {0};
+				DWORD rdLen = 0;
+				
+				while(::ReadFile(hInFile, buff, lengthof(buff), &rdLen, NULL) && rdLen > 0)
+				{
+					DWORD wtLen = 0;
+					::WriteFile(hTmpFile, buff, rdLen, &wtLen, NULL);
+					if( rdLen != wtLen)
+					{
+						dbg_log(_T("fatal write error: %x"), GetLastError());
+						return -1;
+					}
+					dbg_log(_T("write len: %d"), wtLen);
+				}
+				
+				// 得到文件大小
+				// DWORD fileSizeHigh = 0;
+				// DWORD fileSizeLow  = ::GetFileSize(hInFile, &fileSizeHigh);
+				// if(  fileSizeLow ==  INVALID_FILE_SIZE)
+				// {
+					// DbgPrintf(_T("error: GetFileSize failed\n"));
+				// }
+				// else
+				// {
+					// DbgPrintf(L"high:%d\tlow:%d", fileSizeHigh, fileSizeLow);
+					// DbgPrintf(L"%s[%d]:", _T(__FUNCTION__), __LINE__);
+				// }
+				// 删除文件
+				// ::DeleteFile(tmpPath);
 				::CloseHandle(hInFile);
+				::CloseHandle(hTmpFile);
 			}
 			
 		}
