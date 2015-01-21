c0965a86214dd48e4a9cf542da88b507395b2b3b
Simon <hemengsi123@126.com>
2015-01-21 19:53:22 +0800

    update

---
+++ NppUI/NppCombox.cpp
@@ -0,0 +1,168 @@
+
+
+#include "NppLib.h"
+#include "SysMsg.h"
+#include "NppCombox.h"
+
+#include "StdAfx.h"
+
+CNppCombox::CNppCombox()
+{
+	_comboItems.clear();
+}
+
+CNppCombox::~CNppCombox()
+{
+}
+LPCTSTR CNppCombox::getWndClassName()const
+{
+	return WC_COMBOBOX;
+}
+void CNppCombox::init(HINSTANCE hInst, HWND hParent, UINT iCtrlIDs)
+{
+	CNppCtrlWnd::init(hInst, hParent, iCtrlIDs);
+}
+HWND CNppCombox::create(DWORD dwStyle, DWORD dwExStyle, LPCTSTR lpszCaption)
+{
+	HWND hWnd = CNppCtrlWnd::create(dwStyle, dwExStyle, lpszCaption);
+	if( hWnd )
+	{
+		COMBOBOXINFO	comboBoxInfo;
+		comboBoxInfo.cbSize = sizeof(COMBOBOXINFO);
+		::SendMessage(m_hSelf, CB_GETCOMBOBOXINFO, 0, (LPARAM)&comboBoxInfo);
+		CNppWnd::setWndProc(comboBoxInfo.hwndItem);
+	}
+	return hWnd;
+}
+using std::vector;
+
+LRESULT CNppCombox::runCtrlProc(UINT uMsg, WPARAM wParam, LPARAM lParam, OUT bool & bDone)
+{
+	bDone = false;
+	
+	switch( uMsg )
+	{
+		case WM_KEYUP:
+		{
+			dbg_log(_T("uMsg = 0x%04X"), uMsg);
+			// 13 ENTER
+			if (wParam == 13)
+			{
+				LPTSTR	pszText	= (LPTSTR)new TCHAR[MAX_PATH];
+
+				getText(pszText);
+				addText(pszText);
+				::SendMessage(::GetParent(getHSelf()), EXM_CHANGECOMBO, 0, 0);
+				delete [] pszText;
+				bDone = true;
+				return TRUE;
+			}
+//			dbg_log(_T("combo hwnd = 0x%08X"), getHSelf());
+//			::SendMessage(::GetParent(getHSelf()), uMsg, wParam, lParam);
+			bDone = true;
+			break;
+		}
+		case WM_DESTROY:
+		{
+			_comboItems.clear();
+			break;
+		}
+		default :
+			bDone = false;
+			break;
+	}
+}
+
+void CNppCombox::addText(LPCTSTR pszText)
+{
+	/* find item */
+	INT		count		= _comboItems.size();
+	INT		i			= 0;
+	INT		hasFoundOn	= -1;
+
+	for (; i < count; i++)
+	{
+		if (_tcscmp(pszText, _comboItems[i].c_str()) == 0)
+		{
+			hasFoundOn = count - i - 1;
+		}
+	}
+
+	/* item missed create new and select it correct */
+	if (hasFoundOn == -1)
+	{
+		_comboItems.push_back(pszText);
+
+		::SendMessage(getHSelf(), CB_RESETCONTENT, 0, 0);
+		for (i = count; i >= 0 ; --i)
+		{
+			::SendMessage( getHSelf(), CB_ADDSTRING, MAX_PATH, (LPARAM)_comboItems[i].c_str());
+		}
+	}
+	selectComboText(pszText);
+}
+
+
+void CNppCombox::setText(LPCTSTR pszText, UINT size)
+{
+	::SendMessage(getHSelf(), WM_SETTEXT, size, (LPARAM)pszText);
+}
+
+void CNppCombox::getText(LPTSTR pszText, UINT size)
+{
+	::SendMessage(getHSelf(), WM_GETTEXT, size, (LPARAM)pszText);
+}
+
+bool CNppCombox::getSelText(LPTSTR pszText)
+{
+	INT		curSel = ::SendMessage(getHSelf(), CB_GETCURSEL, 0, 0);
+
+	if (curSel != CB_ERR)
+	{
+		if (MAX_PATH > ::SendMessage(getHSelf(), CB_GETLBTEXTLEN, curSel, 0))
+		{
+			::SendMessage(getHSelf(), CB_GETLBTEXT, curSel, (LPARAM)pszText);
+			return true;
+		}
+	}
+	return false;
+}
+
+void CNppCombox::selectComboText(LPCTSTR pszText)
+{
+	LRESULT			lResult	= -1;
+
+	lResult = ::SendMessage(getHSelf(), CB_FINDSTRINGEXACT, -1, (LPARAM)pszText);
+	::SendMessage(getHSelf(), CB_SETCURSEL, lResult, 0);
+}
+
+void CNppCombox::setComboList(vector<tstring> vStrList)
+{
+	size_t	iCnt	= vStrList.size();
+
+	::SendMessage(getHSelf(), CB_RESETCONTENT, 0, 0);
+	for (size_t i = 0; i < iCnt; i++)
+	{
+		addText((LPCTSTR)vStrList[i].c_str());
+	}
+}
+
+void CNppCombox::getComboList(vector<tstring> & vStrList)
+{
+	LPTSTR	pszTemp	= (LPTSTR)new TCHAR[MAX_PATH];
+	size_t	iCnt	= ::SendMessage(getHSelf(), CB_GETCOUNT, 0, 0);
+
+	vStrList.clear();
+
+	for (size_t i = 0; i < iCnt; i++)
+	{
+		if (MAX_PATH > ::SendMessage(getHSelf(), CB_GETLBTEXTLEN, i, 0))
+		{
+			::SendMessage(getHSelf(), CB_GETLBTEXT, i, (LPARAM)pszTemp);
+			vStrList.push_back(pszTemp);
+		}
+	}
+	delete [] pszTemp;
+}
+
+
---
+++ NppUI/NppCombox.h
@@ -0,0 +1,47 @@
+#ifndef COMBOX_DEFINE_H
+#define COMBOX_DEFINE_H
+
+
+#ifndef CB_GETCOMBOBOXINFO
+#define	CB_GETCOMBOBOXINFO	0x0164
+#endif
+
+#if(WINVER <= 0x0400)
+struct COMBOBOXINFO 
+{
+    int cbSize;
+    RECT rcItem;
+    RECT rcButton;
+    DWORD stateButton;
+    HWND hwndCombo;
+    HWND hwndItem;
+    HWND hwndList; 
+};
+#endif 
+
+class CNppCombox: public CNppCtrlWnd
+{
+public :
+	CNppCombox();
+    ~CNppCombox();
+	LPCTSTR getWndClassName()const;
+	void init(HINSTANCE hInst, HWND hParent, UINT iCtrlIDs);
+	HWND create(DWORD dwStyle =  CBS_DROPDOWN, DWORD dwExStyle = 0, LPCTSTR lpszCaption = NULL);
+	void addText(LPCTSTR pszText);
+	void setText(LPCTSTR pszText, UINT size = MAX_PATH);
+	void getText(LPTSTR pszText, UINT size = MAX_PATH);
+	bool getSelText(LPTSTR pszText);
+
+	void setComboList(std::vector<tstring> vStrList);
+	void getComboList(std::vector<tstring> & vStrList);
+	
+	virtual LRESULT runCtrlProc(UINT uMsg, WPARAM wParam, LPARAM lParam, OUT bool & bDone);
+private:
+	void selectComboText(LPCTSTR pszText);
+
+private :
+	tstring					_currData;
+	std::vector<tstring>	_comboItems;
+};
+
+#endif // COMBOORGI_DEFINE_H
